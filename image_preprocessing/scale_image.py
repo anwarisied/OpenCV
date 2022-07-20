6f3f55237643e3cfdb7c11c9e4e9df7eeeaedcd7
Anwar Isied <anwar@imgpro.dev>
2022-07-20 22:56:40 +0300

    Update resize and scale

--- image_preprocessing/scale_image.py
+++ image_preprocessing/scale_image.py
@@ -1,7 +1,7 @@
 
 """
 Resize an image in Python OpenCV
-To resize an image, OpenCV provides cv2.resize() function
+To rescale an image, OpenCV provides cv2.resize() function
 Tested on:
 Python3 on Ubuntu 22.03
 19/07/2022
@@ -9,16 +9,17 @@ Anwar Isied
 """
 import cv2
 
-#Change resoluation of an image with width and height
-def resize_image(img,width,height):
-    
+#Rescale an image or frame, 0..100 (Scale)
+def scale_image(img,scale=50):
+    width = int(img.shape[1] * scale / 100)
+    height = int(img.shape[0] * scale / 100)
+
     dimensions=(width,height)
 
     return cv2.resize(img,dimensions,interpolation=cv2.INTER_AREA)
 
-
 img = cv2.imread('Photos/space.jpg')
-rescaled_image=resize_image(img,500,500)
+rescaled_image=scale_image(img,10)
 
 cv2.imshow('Space',rescaled_image)
 k = cv2.waitKey(0) & 0xFF
